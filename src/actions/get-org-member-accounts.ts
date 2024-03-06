3b6f4ae6caffc9dc0ad300d961f06053696637f1
George Rolston <grolston@users.noreply.github.com>
2024-03-06 10:50:22 -0500

    increase account return

--- src/actions/get-org-member-accounts.ts
+++ src/actions/get-org-member-accounts.ts
@@ -4,8 +4,11 @@ import { OrgMemberAccount } from '../types';
 async function getOrgMemberAccounts(): Promise<OrgMemberAccount[]> {
   let orgMemberAccountInfo: OrgMemberAccount[] = []
   const orgsClient = new OrganizationsClient({ region: 'us-east-1' });
+  const input = {
+    MaxResults: Number("2000"),
+  };
   try {
-    const response: ListAccountsCommandOutput = await orgsClient.send(new ListAccountsCommand({}));
+    const response: ListAccountsCommandOutput = await orgsClient.send(new ListAccountsCommand(input));
     if(response.Accounts && response.Accounts.length > 0){
     for (const account of response.Accounts){
       let orgMemberAccount: OrgMemberAccount = {
