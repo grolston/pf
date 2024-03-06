68af00280449195739ad85538c32868853130440
George Rolston <grolston@users.noreply.github.com>
2024-03-06 12:08:54 -0500

    nextToken

--- src/actions/get-org-member-accounts.ts
+++ src/actions/get-org-member-accounts.ts
@@ -1,22 +1,36 @@
-import { OrganizationsClient, ListAccountsCommand, DescribeOrganizationalUnitCommand, ListAccountsCommandOutput } from '@aws-sdk/client-organizations';
+import { OrganizationsClient, ListAccountsCommand, ListAccountsCommandOutput } from '@aws-sdk/client-organizations';
 import { OrgMemberAccount } from '../types';
 
 async function getOrgMemberAccounts(): Promise<OrgMemberAccount[]> {
   let orgMemberAccountInfo: OrgMemberAccount[] = []
   const orgsClient = new OrganizationsClient({ region: 'us-east-1' });
   const input = {
-    MaxResults: Number("2000"),
+    MaxResults: Number("200"),
   };
   try {
-    const response: ListAccountsCommandOutput = await orgsClient.send(new ListAccountsCommand(input));
+    let response: ListAccountsCommandOutput = await orgsClient.send(new ListAccountsCommand({}));
     if(response.Accounts && response.Accounts.length > 0){
-    for (const account of response.Accounts){
-      let orgMemberAccount: OrgMemberAccount = {
-        accountName: account.Name,
-        accountEmail: account.Email,
+      for (const account of response.Accounts){
+        let orgMemberAccount: OrgMemberAccount = {
+          accountName: account.Name,
+          accountEmail: account.Email,
+        }
+        orgMemberAccountInfo.push(orgMemberAccount)
       }
-      orgMemberAccountInfo.push(orgMemberAccount)
-    }
+      do {
+        if(response.NextToken){
+          response = await orgsClient.send(new ListAccountsCommand({NextToken: response.NextToken}));
+          if(response.Accounts && response.Accounts.length > 0){
+            for (const account of response.Accounts){
+              let orgMemberAccount: OrgMemberAccount = {
+                accountName: account.Name,
+                accountEmail: account.Email,
+              }
+              orgMemberAccountInfo.push(orgMemberAccount)
+            }
+          }
+        }
+      }while(response.NextToken)
     }
   } catch (error) {
     console.error('Error listing AWS accounts:', error);
